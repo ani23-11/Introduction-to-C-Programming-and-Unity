125e6538b5763be3beade491facff3cfe888e23e
Aniket Mishra <aniket21mishra@gmail.com>
2023-03-26 18:35:26 +0530

    Create firstUnity.cs

---
+++ firstUnity.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class printmessage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hi, Noob");
+    }
+}
